53f22f4a517cebf29b5dea1e4805deffe97b5fb3
GILPYO <rlfvy789@kakao.com>
2024-10-10 02:18:50 +0900

    feat: 로그인 페이지 작업 시작

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,22 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login.tsx";
 const queryClient = new QueryClient();
 
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <App />
-        </QueryClientProvider>
-    </StrictMode>
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
 );
