c9ce18f50e96033a006f5c73b64d33ac62e6a578
GILPYO <rlfvy789@kakao.com>
2024-10-12 03:32:55 +0900

    feat: 이메일 로그인 API 연동 및 리다이렉트 완료

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from "./pages/Login.tsx";
-import Home from "./Home.tsx";
+import Login from "./pages/Login";
+import Home from "./Home";
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
