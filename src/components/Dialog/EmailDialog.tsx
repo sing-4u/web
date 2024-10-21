bc88ed3ae712b38fd47dd25d96e7ce8e6869235f
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-10-21 16:34:21 +0900

    fix : UI 및 로직 수정

--- src/components/Dialog/EmailDialog.tsx
+++ src/components/Dialog/EmailDialog.tsx
@@ -1,25 +1,25 @@
-import { FormProvider, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import Dialog, { DialogProps } from "./Dialog";
 import axios from "axios";
-import FormField from "../FormField";
+import getInputErrorClassName from "../../utils/className";
+import usePasswordToggle from "../../hooks/usePasswordToggle";
 
 const EmailDialog = ({ isOpen, onClose }: DialogProps) => {
-    const methods = useForm({
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm({
         defaultValues: {
             email: "",
-            password: ""
+            password: "",
+            confirmPassword: ""
         }
     });
 
     const onSubmit = async (data: { email: string; password: string }) => {
         const { email, password } = data;
-        if (password === email) {
-            methods.setError("password", {
-                type: "custom",
-                message: "비밀번호가 이메일과 동일합니다."
-            });
-            return;
-        }
+
         try {
             await axios.patch(
                 `${import.meta.env.VITE_API_URL}/users/me/email`,
@@ -33,44 +33,93 @@ const EmailDialog = ({ isOpen, onClose }: DialogProps) => {
                 }
             );
             onClose();
-        } catch (error) {
-            console.error(error);
+        } catch {
+            // error handling
         }
     };
 
+    const { passwordState, handleToggle } = usePasswordToggle();
+
     return (
         <Dialog isOpen={isOpen} onClose={onClose} title="이메일 변경">
-            <FormProvider {...methods}>
-                <form onSubmit={methods.handleSubmit(onSubmit)}>
-                    <FormField
-                        label="새 이메일"
-                        name="email"
-                        type="email"
-                        placeholder="이메일을 입력하세요"
-                        rules={{ required: "이메일을 입력해주세요" }}
-                    />
-                    <FormField
-                        label="비밀번호"
-                        name="password"
-                        type="password"
-                        placeholder="비밀번호를 입력하세요"
-                        rules={{
-                            required: "비밀번호를 입력해주세요",
-                            pattern: {
-                                value: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/,
-                                message:
-                                    "비밀번호 취약: 비밀번호는 8~16자의 영문 대/소문자, 숫자, 특수문자를 모두 포함해야 합니다."
-                            }
-                        }}
-                    />
-                    <button
-                        type="submit"
-                        className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        변경하기
-                    </button>
-                </form>
-            </FormProvider>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="space-y-4">
+                    <label className="block text-sm font-medium text-gray-700">
+                        이메일
+                    </label>
+
+                    <div className="flex flex-col">
+                        <input
+                            {...register("email", {
+                                required: "이메일은 필수값입니다.",
+                                pattern: {
+                                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                                    message: "올바른 이메일 형식이 아닙니다."
+                                }
+                            })}
+                            type="email"
+                            className={getInputErrorClassName(errors.email)}
+                            placeholder="이메일을 입력해주세요."
+                        />
+                        {errors.email && (
+                            <p className="text-red-500">
+                                {errors.email.message}
+                            </p>
+                        )}
+                    </div>
+                </div>
+
+                <div className="space-y-4">
+                    <label className="block text-sm font-medium text-gray-700">
+                        비밀번호
+                    </label>
+                    <div className="flex flex-col">
+                        <input
+                            {...register("password", {
+                                required: "비밀번호는 필수값입니다.",
+                                pattern: {
+                                    value: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/,
+                                    message:
+                                        "비밀번호 취약: 비밀번호는 8~16자의 영문 대/소문자, 숫자, 특수문자를 모두 포함해야 합니다."
+                                }
+                            })}
+                            type={passwordState.type}
+                            className={getInputErrorClassName(errors.password)}
+                            placeholder="비밀번호를 입력해주세요."
+                        />
+                        {errors.password && (
+                            <p className="text-red-500">
+                                {errors.password.message?.toString()}
+                            </p>
+                        )}
+                        {passwordState.type === "password" && (
+                            <div className="absolute inset-y-0 right-0 pr-3 flex items-center hover:cursor-pointer">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-5 w-5 text-gray-400"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    onClick={handleToggle}
+                                >
+                                    <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                        )}
+                    </div>
+                </div>
+
+                <button
+                    type="submit"
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    변경하기
+                </button>
+            </form>
         </Dialog>
     );
 };
--- src/components/Dialog/PasswordDialog.tsx
+++ src/components/Dialog/PasswordDialog.tsx
@@ -1,10 +1,23 @@
-import { FormProvider, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import Dialog, { DialogProps } from "./Dialog";
 import axios from "axios";
-import FormField from "../FormField";
+import getInputErrorClassName from "../../utils/className";
 
 const PasswordDialog = ({ isOpen, onClose }: DialogProps) => {
-    const methods = useForm({
+    // const methods = useForm({
+    //     defaultValues: {
+    //         oldPassword: "",
+    //         newPassword: "",
+    //         confirmPassword: ""
+    //     }
+    // });
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        watch
+    } = useForm({
         defaultValues: {
             oldPassword: "",
             newPassword: "",
@@ -12,26 +25,16 @@ const PasswordDialog = ({ isOpen, onClose }: DialogProps) => {
         }
     });
 
+    const watchPassword = watch("newPassword");
+    const watchOldPassword = watch("oldPassword");
+
     const onSubmit = async (data: {
         oldPassword: string;
         newPassword: string;
         confirmPassword: string;
     }) => {
-        const { oldPassword, newPassword, confirmPassword } = data;
-        if (newPassword !== confirmPassword) {
-            methods.setError("confirmPassword", {
-                type: "custom",
-                message: "새 비밀번호가 일치하지 않습니다."
-            });
-            return;
-        }
-        if (newPassword === oldPassword) {
-            methods.setError("newPassword", {
-                type: "custom",
-                message: "새 비밀번호가 현재 비밀번호와 동일합니다."
-            });
-            return;
-        }
+        const { oldPassword, newPassword } = data;
+
         try {
             await axios.patch(
                 `${import.meta.env.VITE_API_URL}/users/me/password`,
@@ -45,51 +48,108 @@ const PasswordDialog = ({ isOpen, onClose }: DialogProps) => {
                 }
             );
             onClose();
-        } catch (error) {
-            console.error(error);
+        } catch {
+            // error handling
         }
     };
 
     return (
         <Dialog isOpen={isOpen} onClose={onClose} title="비밀번호 변경">
-            <FormProvider {...methods}>
-                <form onSubmit={methods.handleSubmit(onSubmit)}>
-                    <FormField
-                        label="현재 비밀번호"
-                        name="oldPassword"
-                        type="password"
-                        placeholder="현재 비밀번호 입력"
-                        rules={{ required: "현재 비밀번호를 입력해주세요" }}
-                    />
-                    <FormField
-                        label="새 비밀번호"
-                        name="newPassword"
-                        type="password"
-                        placeholder="새 비밀번호 입력"
-                        rules={{
-                            required: "새 비밀번호를 입력해주세요",
-                            pattern: {
-                                value: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/,
-                                message:
-                                    "비밀번호 취약: 비밀번호는 8~16자의 영문 대/소문자, 숫자, 특수문자를 모두 포함해야 합니다."
-                            }
-                        }}
-                    />
-                    <FormField
-                        label="새 비밀번호 확인"
-                        name="confirmPassword"
-                        type="password"
-                        placeholder="새 비밀번호 확인"
-                        rules={{ required: "새 비밀번호를 다시 입력해주세요" }}
-                    />
-                    <button
-                        type="submit"
-                        className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        변경하기
-                    </button>
-                </form>
-            </FormProvider>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="space-y-4">
+                    <label className="block text-sm font-medium text-gray-700">
+                        현재 비밀번호
+                    </label>
+                    <div className="flex flex-col">
+                        <input
+                            {...register("oldPassword", {
+                                required: "비밀번호는 필수값입니다.",
+                                pattern: {
+                                    value: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/,
+                                    message:
+                                        "비밀번호 취약: 비밀번호는 8~16자의 영문 대/소문자, 숫자, 특수문자를 모두 포함해야 합니다."
+                                },
+                                validate: (value) =>
+                                    value !== watchOldPassword ||
+                                    "새 비밀번호와 현재 비밀번호가 일치합니다. 다시 확인해주세요."
+                            })}
+                            type="password"
+                            className={getInputErrorClassName(
+                                errors.oldPassword
+                            )}
+                            placeholder="비밀번호를 입력해주세요."
+                        />
+                        {errors.oldPassword && (
+                            <p className="text-red-500">
+                                {errors.oldPassword.message}
+                            </p>
+                        )}
+                    </div>
+                </div>
+                <div className="space-y-4">
+                    <label className="block text-sm font-medium text-gray-700">
+                        새 비밀번호
+                    </label>
+                    <div className="flex flex-col">
+                        <input
+                            {...register("newPassword", {
+                                required: "비밀번호는 필수값입니다.",
+                                pattern: {
+                                    value: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/,
+                                    message:
+                                        "비밀번호 취약: 비밀번호는 8~16자의 영문 대/소문자, 숫자, 특수문자를 모두 포함해야 합니다."
+                                }
+                            })}
+                            type="password"
+                            className={getInputErrorClassName(
+                                errors.newPassword
+                            )}
+                            placeholder="새 비밀번호를 입력해주세요."
+                        />
+                        {errors.newPassword && (
+                            <p className="text-red-500">
+                                {errors.newPassword.message}
+                            </p>
+                        )}
+                    </div>
+                </div>
+                <div className="space-y-4">
+                    <label className="block text-sm font-medium text-gray-700">
+                        새 비밀번호 확인
+                    </label>
+                    <div className="flex flex-col">
+                        <input
+                            {...register("confirmPassword", {
+                                required: "비밀번호는 필수값입니다.",
+                                pattern: {
+                                    value: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/,
+                                    message:
+                                        "비밀번호 취약: 비밀번호는 8~16자의 영문 대/소문자, 숫자, 특수문자를 모두 포함해야 합니다."
+                                },
+                                validate: (value) =>
+                                    value === watchPassword ||
+                                    "비밀번호가 일치하지 않습니다."
+                            })}
+                            type="password"
+                            className={getInputErrorClassName(
+                                errors.confirmPassword
+                            )}
+                            placeholder="새 비밀번호를 다시 입력해주세요."
+                        />
+                        {errors.confirmPassword && (
+                            <p className="text-red-500">
+                                {errors.confirmPassword.message?.toString()}
+                            </p>
+                        )}
+                    </div>
+                </div>
+                <button
+                    type="submit"
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    변경하기
+                </button>
+            </form>
         </Dialog>
     );
 };
