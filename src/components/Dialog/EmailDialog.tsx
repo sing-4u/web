0352e3cce47845b9cecb62345b595608584dede5
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-10-21 19:53:15 +0900

    fix : UI 수정

--- src/components/Dialog/EmailDialog.tsx
+++ src/components/Dialog/EmailDialog.tsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
-import axiosInstance from "../../utils/axiosInstance";
 import getInputErrorClassName from "../../utils/className";
+import usePasswordToggle from "../../hooks/usePasswordToggle";
+import axiosInstance from "../../utils/axiosInstance";
 import { DialogContentProps } from "../../types";
 
 const EmailDialogContent = ({ onClose }: DialogContentProps) => {
@@ -9,18 +10,25 @@ const EmailDialogContent = ({ onClose }: DialogContentProps) => {
         handleSubmit,
         formState: { errors }
     } = useForm({
-        defaultValues: { email: "", password: "" }
+        defaultValues: {
+            email: "",
+            password: "",
+            confirmPassword: ""
+        }
     });
 
     const onSubmit = async (data: { email: string; password: string }) => {
         const { email, password } = data;
+
         try {
-            await axiosInstance.patch("/users/me/email", { email, password });
+            axiosInstance.patch("/users/me/email", { email, password });
             onClose();
         } catch {
             // error handling
         }
     };
+
+    const { passwordState, handleToggle } = usePasswordToggle();
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
@@ -28,6 +36,7 @@ const EmailDialogContent = ({ onClose }: DialogContentProps) => {
                 <label className="mb-2 block text-sm font-medium text-gray-700">
                     이메일
                 </label>
+
                 <div className="flex flex-col">
                     <input
                         {...register("email", {
@@ -48,11 +57,12 @@ const EmailDialogContent = ({ onClose }: DialogContentProps) => {
                     ) : null}
                 </div>
             </div>
+
             <div>
                 <label className="my-2 block text-sm font-medium text-gray-700">
                     비밀번호
                 </label>
-                <div className="flex flex-col">
+                <div className="flex flex-col relative">
                     <input
                         {...register("password", {
                             required: "비밀번호는 필수값입니다.",
@@ -62,8 +72,8 @@ const EmailDialogContent = ({ onClose }: DialogContentProps) => {
                                     "비밀번호 취약: 비밀번호는 8~16자의 영문 대/소문자, 숫자, 특수문자를 모두 포함해야 합니다."
                             }
                         })}
-                        type="password"
-                        className={`mb-2 ${getInputErrorClassName(
+                        type={passwordState.type}
+                        className={`mb-2 w-full ${getInputErrorClassName(
                             errors.password
                         )}`}
                         placeholder="비밀번호를 입력해주세요."
@@ -73,8 +83,27 @@ const EmailDialogContent = ({ onClose }: DialogContentProps) => {
                             {errors.password.message}
                         </p>
                     ) : null}
+                    {passwordState.type === "password" && (
+                        <div className="absolute top-4 right-0 pr-3 flex items-center hover:cursor-pointer">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5 text-gray-400"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                onClick={handleToggle}
+                            >
+                                <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                                <path
+                                    fillRule="evenodd"
+                                    d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        </div>
+                    )}
                 </div>
             </div>
+
             <button
                 type="submit"
                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
--- src/components/Dialog/PasswordDialog.tsx
+++ src/components/Dialog/PasswordDialog.tsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import axiosInstance from "../../utils/axiosInstance";
 import getInputErrorClassName from "../../utils/className";
 import { DialogContentProps } from "../../types";
+import usePasswordToggle from "../../hooks/usePasswordToggle";
 
 const PasswordDialogContent = ({ onClose }: DialogContentProps) => {
     const {
@@ -33,13 +34,21 @@ const PasswordDialogContent = ({ onClose }: DialogContentProps) => {
         }
     };
 
+    const { passwordState, handleToggle } = usePasswordToggle();
+    const {
+        passwordState: confirmPasswordState,
+        handleToggle: handleConfirmToggle
+    } = usePasswordToggle();
+    const { passwordState: oldPasswordState, handleToggle: handleOldToggle } =
+        usePasswordToggle();
+
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <div>
                 <label className="mb-2 block text-sm font-medium text-gray-700">
                     현재 비밀번호
                 </label>
-                <div className="flex flex-col">
+                <div className="flex flex-col relative">
                     <input
                         {...register("oldPassword", {
                             required: "비밀번호는 필수값입니다.",
@@ -60,13 +69,31 @@ const PasswordDialogContent = ({ onClose }: DialogContentProps) => {
                             {errors.oldPassword.message}
                         </p>
                     )}
+                    {oldPasswordState.type === "password" && (
+                        <div className="absolute top-4 right-0 pr-3 flex items-center hover:cursor-pointer">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5 text-gray-400"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                onClick={handleOldToggle}
+                            >
+                                <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                                <path
+                                    fillRule="evenodd"
+                                    d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        </div>
+                    )}
                 </div>
             </div>
             <div>
                 <label className="mb-2 block text-sm font-medium text-gray-700">
                     새 비밀번호
                 </label>
-                <div className="flex flex-col">
+                <div className="flex flex-col relative">
                     <input
                         {...register("newPassword", {
                             required: "비밀번호는 필수값입니다.",
@@ -90,13 +117,31 @@ const PasswordDialogContent = ({ onClose }: DialogContentProps) => {
                             {errors.newPassword.message}
                         </p>
                     )}
+                    {passwordState.type === "password" && (
+                        <div className="absolute top-4 right-0 pr-3 flex items-center hover:cursor-pointer">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5 text-gray-400"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                onClick={handleToggle}
+                            >
+                                <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                                <path
+                                    fillRule="evenodd"
+                                    d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        </div>
+                    )}
                 </div>
             </div>
             <div>
                 <label className="mb-2 block text-sm font-medium text-gray-700">
                     새 비밀번호 확인
                 </label>
-                <div className="flex flex-col">
+                <div className="flex flex-col relative">
                     <input
                         {...register("confirmPassword", {
                             required: "비밀번호는 필수값입니다.",
@@ -114,6 +159,24 @@ const PasswordDialogContent = ({ onClose }: DialogContentProps) => {
                         <p className="text-red-500">
                             {errors.confirmPassword.message}
                         </p>
+                    )}
+                    {confirmPasswordState.type === "password" && (
+                        <div className="absolute top-4 right-0 pr-3 flex items-center hover:cursor-pointer">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5 text-gray-400"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                onClick={handleConfirmToggle}
+                            >
+                                <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                                <path
+                                    fillRule="evenodd"
+                                    d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        </div>
                     )}
                 </div>
             </div>
