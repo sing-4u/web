c2541a324084fa773ca48f427d6ac6c52aa63adb
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-10-25 14:56:50 +0900

    refactor : bgColorClass

--- src/components/ToastContainer.tsx
+++ src/components/ToastContainer.tsx
@@ -8,21 +8,25 @@ export const ToastContainer = ({ toasts }: { toasts: ToastProps[] }) => {
     return (
         <div className="fixed top-4 right-[40%] space-y-2 z-50">
             {toasts.map((toast) => (
-                <div
-                    key={toast.id}
-                    className={`p-4 rounded-md text-white shadow-lg transition animate-bounceIn ${
-                        toast.type === "success"
-                            ? "bg-green-500"
-                            : toast.type === "error"
-                            ? "bg-red-500"
-                            : toast.type === "warning"
-                            ? "bg-yellow-500"
-                            : "bg-blue-500"
-                    }`}
-                >
-                    {toast.message}
-                </div>
+                <Toast key={toast.id} {...toast} />
             ))}
+        </div>
+    );
+};
+
+const Toast = ({ type, message }: ToastProps) => {
+    const bgColorClass = {
+        success: "bg-green-500",
+        error: "bg-red-500",
+        warning: "bg-yellow-500",
+        info: "bg-blue-500"
+    }[type];
+
+    return (
+        <div
+            className={`p-4 rounded-md text-white shadow-lg transition animate-bounceIn ${bgColorClass}`}
+        >
+            {message}
         </div>
     );
 };
