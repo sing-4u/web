9fa67141424684cf5095adc0f6bb4e28c0e30524
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-10-26 07:12:01 +0900

    fix : 이미지 수정

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,5 @@
 import { useModal } from "../../hooks/useModal";
+import CloseButton from "../../../src/assets/btn_close.svg";
 
 export interface DialogProps {
     isOpen?: boolean;
@@ -16,21 +17,8 @@ const Modal = ({ isOpen, title, children }: DialogProps) => {
             <div className="bg-white p-6 rounded-lg w-[328px]">
                 <div className="flex justify-between items-center mb-4">
                     <h2 className="text-lg font-bold">{title}</h2>
-                    <button onClick={closeModal} className="text-gray-500">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M6 18L18 6M6 6l12 12"
-                            />
-                        </svg>
+                    <button onClick={closeModal}>
+                        <img src={CloseButton} alt="close" />
                     </button>
                 </div>
                 {children}
