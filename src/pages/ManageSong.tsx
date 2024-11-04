30ae7eee607e90a4dceb76b8fb30f7302fda2fb3
GILPYO <rlfvy789@kakao.com>
2024-11-05 02:08:02 +0900

    feat: 신청곡 상태에따라 프로필 이미지 테두리 변경 테스트 완료

--- src/pages/ManageSong.tsx
+++ src/pages/ManageSong.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import ImgProfileL from "../components/ImgProfileL";
 import useUserData from "../hooks/useUserData";
@@ -8,6 +8,16 @@ const ManageSong = () => {
   const profileImage = userData?.image;
   const nickname = userData?.name;
 
+  const [receivingSong, setReceivingSong] = useState(false);
+
+  const handleReceivingSong = () => {
+    setReceivingSong(true);
+  };
+
+  const handleEndReceivingSong = () => {
+    setReceivingSong(false);
+  };
+
   const smallButtonClass =
     "w-[160px] h-[44px] rounded-[4px] py-3.5 px-5 font-semibold text-[14px] leading-[16.71px]";
 
@@ -15,7 +25,13 @@ const ManageSong = () => {
     <div className="w-full max-w-[376px] mx-auto flex flex-col items-center">
       <Navbar />
       <div className="relative w-[90px] h-[90px] mt-6">
-        <div className="absolute inset-0 p-[4px] bg-gradient-to-r from-[#7B92C7] via-[#7846DD] to-[#BB7FA0] rounded-full">
+        <div
+          className={`absolute inset-0 p-[4px] rounded-full ${
+            receivingSong
+              ? "bg-gradient-to-r from-[#7B92C7] via-[#7846DD] to-[#BB7FA0]"
+              : ""
+          }`}
+        >
           <div className="relative w-full h-full bg-white rounded-full overflow-hidden">
             {profileImage ? (
               <img
@@ -28,16 +44,31 @@ const ManageSong = () => {
             )}
           </div>
         </div>
+        {receivingSong && (
+          <p
+            className="absolute flex justify-center items-center bottom-[-10px] left-1/2 transform -translate-x-1/2
+          w-[73px] h-[20px] rounded-[4px] py-1 px-2 font-semibold text-[10px] leading-[11.93px]
+           bg-gradient-to-r from-[#7B92C7] via-[#7846DD] to-[#BB7FA0] text-white"
+          >
+            신청곡 받는 중
+          </p>
+        )}
       </div>
       <div className="mt-4 font-semibold text-[18px] leading-[21.48px]">
         {nickname}
       </div>
       <div className="flex flex-col items-center mt-4">
         <div className="flex space-x-2">
-          <button className={smallButtonClass + " bg-buttonColor2"}>
+          <button
+            onClick={handleReceivingSong}
+            className={smallButtonClass + " bg-buttonColor2"}
+          >
             신청곡 받기
           </button>
-          <button className={smallButtonClass + " bg-black text-white"}>
+          <button
+            onClick={handleEndReceivingSong}
+            className={smallButtonClass + " bg-black text-white"}
+          >
             신청곡 종료
           </button>
         </div>
