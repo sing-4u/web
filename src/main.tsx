c918a01a4102e1575b0dcfcc02d9c38be0cc59aa
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-11-04 19:49:10 +0900

    fix : findPassword로 수정

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import CompleteJoin from "./pages/CompleteJoin";
 import { ModalProvider } from "./components/Modal/ModalProvider";
 import NewPassword from "./pages/NewPassword";
 import Mypage from "./pages/Mypage";
-import FindPassword from "./pages/FindPassword";
+import FindPassword from "./pages/findPassword";
 
 const queryClient = new QueryClient();
 
