eafc3bb9f183dfca8a1a37f4b88282332a4bc48c
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-11-09 22:08:01 +0900

    fix : backgroundColor 네이밍 수정

--- src/components/Modal/EmailModal.tsx
+++ src/components/Modal/EmailModal.tsx
@@ -105,7 +105,7 @@ const EmailModalContent = () => {
 
             <button
                 type="submit"
-                className="mt-8 w-full h-[52px] flex justify-center items-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-buttonBackgroundColor hover:opacity-80 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="mt-8 w-full h-[52px] flex justify-center items-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium  bg-colorPurple text-white hover:opacity-80 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
                 변경하기
             </button>
--- src/components/Modal/PasswordModal.tsx
+++ src/components/Modal/PasswordModal.tsx
@@ -180,7 +180,7 @@ const PasswordModalContent = () => {
 
             <button
                 type="submit"
-                className="mt-8 w-full h-[52px] flex justify-center items-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-buttonBackgroundColor hover:opacity-80 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="mt-8 w-full h-[52px] flex justify-center items-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-colorPurple hover:opacity-80 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
                 변경하기
             </button>
