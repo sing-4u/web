8c9e41e83b366386504140ed103f9b2281e63fa3
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-11-10 15:04:01 +0900

    fix : accessToken이 존재할 때만 useUserData 호출하도록 수정

--- src/hooks/useUserData.tsx
+++ src/hooks/useUserData.tsx
@@ -2,41 +2,45 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface UserData {
-  id: string;
-  name: string;
-  email: string;
-  image: string;
-  isOpened: true;
-  provider: "EMAIL" | string;
+    id: string;
+    name: string;
+    email: string;
+    image: string;
+    isOpened: true;
+    provider: "EMAIL" | string;
 }
 
 const useUserData = () => {
-  return useQuery<UserData>({
-    queryKey: ["user"],
-    queryFn: async () => {
-      try {
-        const response = await axios.get<UserData>(
-          `${import.meta.env.VITE_API_URL}/users/me`,
-          {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-            },
-          }
-        );
-        return response.data;
-      } catch (error) {
-        if (axios.isAxiosError(error)) {
-          if (error.response?.status === 400) {
-            console.error("유효성 검사 실패");
-          } else if (error.response?.status === 401) {
-            console.error("인증 실패");
-          }
-        }
-        console.error("유저 정보 가져오기 실패");
-        throw error;
-      }
-    },
-  });
+    const accessToken = localStorage.getItem("accessToken");
+    return useQuery<UserData>({
+        queryKey: ["user"],
+        queryFn: async () => {
+            try {
+                const response = await axios.get<UserData>(
+                    `${import.meta.env.VITE_API_URL}/users/me`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem(
+                                "accessToken"
+                            )}`
+                        }
+                    }
+                );
+                return response.data;
+            } catch (error) {
+                if (axios.isAxiosError(error)) {
+                    if (error.response?.status === 400) {
+                        console.error("유효성 검사 실패");
+                    } else if (error.response?.status === 401) {
+                        console.error("인증 실패");
+                    }
+                }
+                console.error("유저 정보 가져오기 실패");
+                throw error;
+            }
+        },
+        enabled: Boolean(accessToken)
+    });
 };
 
 export default useUserData;
