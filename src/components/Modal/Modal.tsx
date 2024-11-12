8f1bb574422d4f8b5ea2cf0f0ea5012780fbf79d
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-11-12 19:42:45 +0900

    fix : ModalType에 따른 분기 처리

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,10 +3,9 @@ import { ModalContentProps, ModalType } from "../../types";
 import CloseButton from "../../../src/assets/btn_close.svg";
 
 interface ModalProps<T> {
-    onClose: () => void;
+    onClose?: () => void;
     title?: string;
     Content?: ComponentType<ModalContentProps<T>>;
-    // errorMessage?: string;
     type: ModalType;
     data?: T;
     buttonBackgroundColor?: string;
@@ -22,12 +21,17 @@ export const Modal = <T,>({
 }: ModalProps<T>) => {
     if (!Content) return null;
 
+    const buttonClassName = `w-full py-3 ${buttonBackgroundColor} text-textColor rounded-lg mt-4`;
+
+    const onClickModal = (event: React.MouseEvent<HTMLElement>) => {
+        if (event.target === event.currentTarget) {
+            onClose?.();
+        }
+    };
     return (
         <div
             className="fixed inset-0 bg-transparent p-0"
-            onClick={(e) => {
-                if (e.target === e.currentTarget) onClose();
-            }}
+            onClick={onClickModal}
         >
             <div className="fixed inset-0 bg-black/50" aria-hidden="true" />
             <div className="fixed inset-0 flex items-center justify-center p-4">
@@ -35,52 +39,34 @@ export const Modal = <T,>({
                     className="bg-white p-6 rounded-lg w-[328px] relative"
                     open
                 >
-                    {/* <div
+                    <div
                         className={`flex ${
-                            errorMessage ? "justify-center" : "justify-between"
+                            type === ModalType.ERROR
+                                ? "justify-center"
+                                : "justify-between"
                         } items-center mb-4`}
                     >
-                        <h2 className="text-lg font-bold">
-                            {errorMessage || title}
-                        </h2>
-                        {errorMessage === "" && (
-                            <button onClick={onClose}>
-                                <img src={CloseButton} alt="Close" />
-                            </button>
-                        )}
-                    </div> */}
-                    <div className="flex justify-between items-center mb-4">
                         <h2 className="text-lg font-bold">{title}</h2>
+                        {/* 성공도 실패도 아닐 경우(이메일 변경 모달, 비밀번호 변경 모달) */}
                         {type === ModalType.DEFAULT && (
                             <button onClick={onClose}>
                                 <img src={CloseButton} alt="Close" />
                             </button>
                         )}
+                        {/*  */}
                     </div>
                     <Content
-                        title={title}
                         data={data}
                         buttonBackgroundColor={buttonBackgroundColor}
                     />
 
-                    {/* {!errorMessage ? (
-                        <div className="text-red-500 text-sm mt-2">
-                            {errorMessage}
-                        </div>
-                    ) : (
-                        <button
-                            onClick={onClose}
-                            className="w-full py-3 bg-colorPurple text-textColor rounded-lg mt-4"
-                        >
+                    {type === ModalType.ERROR && (
+                        <button onClick={onClose} className={buttonClassName}>
                             확인
                         </button>
-                    )} */}
-
-                    {type === ModalType.ERROR && (
-                        <button
-                            onClick={onClose}
-                            className="w-full py-3 bg-colorPurple text-textColor rounded-lg mt-4"
-                        >
+                    )}
+                    {type === ModalType.DEFAULT && (
+                        <button onClick={onClose} className={buttonClassName}>
                             확인
                         </button>
                     )}
