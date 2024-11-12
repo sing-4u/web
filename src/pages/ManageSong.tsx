3c4945be2740ffa1960c5a6eb0a98a43799bd12a
GILPYO <rlfvy789@kakao.com>
2024-11-12 14:56:52 +0900

    deploy 테스트

--- src/pages/ManageSong.tsx
+++ src/pages/ManageSong.tsx
@@ -13,7 +13,8 @@ import ChevronUp from "../components/ChevronUp";
 import PreviousSongList from "../components/PreviousSongList";
 import axiosInstance from "../utils/axiosInstance";
 
-// 커밋 트리거 테스트
+// 커밋 테스트를 위해 주석 추가
+// deploy 테스트를 위해 주석 추가
 
 const ManageSong = () => {
   const queryClient = useQueryClient();
