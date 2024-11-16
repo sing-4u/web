eed857d5afce27092f1f4fca40f2e38987a4abed
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-11-16 23:38:35 +0900

    fix : 하드코딩 삭제

--- src/utils/className.tsx
+++ src/utils/className.tsx
@@ -2,7 +2,7 @@ import { FieldError } from "react-hook-form";
 
 const getInputErrorClassName = (hasError: FieldError | undefined) => {
     return `border rounded-lg py-[14px] px-[18px] placeholder:text-inputTextColor ${
-        hasError ? "border-[#FF4242]" : "border-[#e1e1e1]"
+        hasError ? "border-[#FF4242]" : "border-inputBorderColor"
     }`;
 };
 
