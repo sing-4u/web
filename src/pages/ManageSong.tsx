67128a76a72a7c4f876c036bfa727c7a971cd2e2
GILPYO <rlfvy789@kakao.com>
2024-11-16 23:58:59 +0900

    feat: 현재신청곡 없을 때 분기처리 및 디자인 완료

--- src/pages/ManageSong.tsx
+++ src/pages/ManageSong.tsx
@@ -171,7 +171,7 @@ const ManageSong = () => {
             </button>
           </div>
         </div>
-        {receivingSong && nowSongList && (
+        {receivingSong && nowSongList ? (
           <div className="flex flex-col w-[327px] rounded-[8px] border-2 border-indigo-500/50 p-4 mt-8">
             <div
               className="flex justify-between items-center cursor-pointer"
@@ -231,6 +231,16 @@ const ManageSong = () => {
                 </button>
               </div>
             )}
+          </div>
+        ) : (
+          <div className="relative border-2 border-solid rounded-[8px] mt-6">
+            <div className="p-3 w-[327px] h-[178px] font-semibold text-[18px] leading-[21.48px]">
+              현재 신청 곡 순위
+              <div className="flex absolute top-[55px] left-[20px] justify-center items-center text-center w-[287px] h-[97px] text-[12px] leading-[16px] border-2 opacity-50">
+                신청곡 게시물이 없습니다.
+                <br /> 신청곡을 받아보세요
+              </div>
+            </div>
           </div>
         )}
         {previousSongLists?.length > 0 && (
