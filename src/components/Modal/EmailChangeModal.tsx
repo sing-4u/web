fbf7cabe78b68446af71c0da311e2004235ab11f
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-11-16 23:38:12 +0900

    fix : 에러메시지 출력 시 아이콘 고정 안되는 부분 수정

--- src/components/Modal/EmailChangeModal.tsx
+++ src/components/Modal/EmailChangeModal.tsx
@@ -43,65 +43,72 @@ const EmailChangeModal = ({
             className="flex flex-col gap-y-4 rounded-[10px]"
         >
             <div>
-                <label className="mb-2 block text-sm font-medium text-gray-700">
-                    이메일
+                <label className="block *:text-sm text-gray-700">
+                    새 이메일
+                    <div className="flex flex-col">
+                        <input
+                            {...register("email", {
+                                required: "이메일은 필수값입니다.",
+                                pattern: {
+                                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                                    message: "올바른 이메일 형식이 아닙니다."
+                                }
+                            })}
+                            type="email"
+                            className={`my-2 ${getInputErrorClassName(
+                                errors.email
+                            )}`}
+                            placeholder="이메일 입력"
+                        />
+                        {errors.email ? (
+                            <p className="text-red-500">
+                                {errors.email.message}
+                            </p>
+                        ) : null}
+                    </div>
                 </label>
-
-                <div className="flex flex-col">
-                    <input
-                        {...register("email", {
-                            required: "이메일은 필수값입니다.",
-                            pattern: {
-                                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                                message: "올바른 이메일 형식이 아닙니다."
-                            }
-                        })}
-                        type="email"
-                        className={`${getInputErrorClassName(errors.email)}`}
-                        placeholder="이메일 입력"
-                    />
-                    {errors.email ? (
-                        <p className="text-red-500">{errors.email.message}</p>
-                    ) : null}
-                </div>
             </div>
 
             <div>
-                <label className="my-2 block text-sm font-medium text-gray-700">
+                <label className="block text-sm text-gray-700">
                     비밀번호
-                </label>
-                <div className="relative">
-                    <input
-                        {...register("password", {
-                            required: "비밀번호는 필수값입니다.",
-                            pattern: {
-                                value: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/,
-                                message:
-                                    "비밀번호 취약: 비밀번호는 8~16자의 영문 대/소문자, 숫자, 특수문자를 모두 포함해야 합니다."
-                            }
-                        })}
-                        type={passwordState.type}
-                        className={`w-full ${getInputErrorClassName(
-                            errors.password
-                        )}`}
-                        placeholder="비밀번호를 입력해주세요."
-                    />
+                    <div className="flex flex-col">
+                        <div className="relative">
+                            <input
+                                {...register("password", {
+                                    required: "비밀번호는 필수값입니다.",
+                                    pattern: {
+                                        value: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/,
+                                        message:
+                                            "비밀번호 취약: 비밀번호는 8~16자의 영문 대/소문자, 숫자, 특수문자를 모두 포함해야 합니다."
+                                    }
+                                })}
+                                type={passwordState.type}
+                                className={`w-full my-2 ${getInputErrorClassName(
+                                    errors.password
+                                )}`}
+                                placeholder="비밀번호를 입력해주세요."
+                            />
+                            {errors.password ? (
+                                <p className="text-red-500">
+                                    {errors.password.message}
+                                </p>
+                            ) : null}
 
-                    <button
-                        type="button"
-                        className="absolute inset-y-0 right-0 pr-3 flex items-center"
-                        onClick={handleToggle}
-                    >
-                        <img
-                            src={handleEyeIconToggle()}
-                            alt="toggle password visibility"
-                            className="h-5 w-5"
-                        />
-                    </button>
-                </div>
-                {errors.password ? (
-                    <p className="text-red-500">{errors.password.message}</p>
-                ) : null}
+                            <button
+                                type="button"
+                                className="absolute top-8 -translate-y-1/2 right-0 pr-3 flex items-center"
+                                onClick={handleToggle}
+                            >
+                                <img
+                                    src={handleEyeIconToggle()}
+                                    alt="toggle password visibility"
+                                    className="h-5 w-5"
+                                />
+                            </button>
+                        </div>
+                    </div>
+                </label>
             </div>
 
             <button
--- src/components/Modal/PasswordChangeModal.tsx
+++ src/components/Modal/PasswordChangeModal.tsx
@@ -64,121 +64,132 @@ const PasswordChangeModal = ({
             className="flex flex-col gap-y-4 rounded-[10px]"
         >
             <div>
-                <label className="mb-2 block text-sm font-medium text-gray-700">
+                <label className="mb-2 block text-sm text-gray-700">
                     현재 비밀번호
+                    <div className="flex flex-col">
+                        <div className="relative">
+                            <input
+                                {...register("oldPassword", {
+                                    required: "비밀번호를 입력해 주세요.",
+                                    pattern: {
+                                        value: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/,
+                                        message:
+                                            "비밀번호 취약: 비밀번호는 8~16자의 영문 대/소문자, 숫자, 특수문자를 모두 포함해야 합니다."
+                                    }
+                                })}
+                                type={oldPasswordState.type}
+                                className={`w-full pr-10 rounded-[10px] mt-2 ${getInputErrorClassName(
+                                    errors.oldPassword
+                                )}`}
+                                placeholder="비밀번호 입력"
+                            />
+                            {errors.oldPassword && (
+                                <p className="mt-1 text-sm text-red-500">
+                                    {errors.oldPassword.message}
+                                </p>
+                            )}
+                            <button
+                                type="button"
+                                className="absolute top-8 -translate-y-1/2 right-0 pr-3 flex items-center"
+                                onClick={handleOldToggle}
+                            >
+                                <img
+                                    src={handleOldEyeIconToggle()}
+                                    alt="toggle password visibility"
+                                    className="h-5 w-5"
+                                />
+                            </button>
+                        </div>
+                    </div>
                 </label>
-                <div className="relative">
-                    <input
-                        {...register("oldPassword", {
-                            required: "비밀번호는 필수값입니다.",
-                            pattern: {
-                                value: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/,
-                                message:
-                                    "비밀번호 취약: 비밀번호는 8~16자의 영문 대/소문자, 숫자, 특수문자를 모두 포함해야 합니다."
-                            }
-                        })}
-                        type={oldPasswordState.type}
-                        className={`w-full pr-10 rounded-[10px] border-inputBorderColor ${getInputErrorClassName(
-                            errors.oldPassword
-                        )}`}
-                        placeholder="비밀번호 입력"
-                    />
-                    <button
-                        type="button"
-                        className="absolute inset-y-0 right-0 pr-3 flex items-center"
-                        onClick={handleOldToggle}
-                    >
-                        <img
-                            src={handleOldEyeIconToggle()}
-                            alt="toggle password visibility"
-                            className="h-5 w-5"
-                        />
-                    </button>
-                </div>
-                {errors.oldPassword && (
-                    <p className="mt-1 text-sm text-red-500">
-                        {errors.oldPassword.message}
-                    </p>
-                )}
             </div>
 
             <div>
-                <label className="mb-2 block text-sm font-medium text-gray-700">
+                <label className="mb-2 block text-sm text-gray-700">
                     새 비밀번호
+                    <div className="flex flex-col">
+                        <div className="relative">
+                            <input
+                                {...register("newPassword", {
+                                    required: "비밀번호를 입력해 주세요.",
+                                    pattern: {
+                                        value: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/,
+                                        message:
+                                            "비밀번호 취약: 비밀번호는 8~16자의 영문 대/소문자, 숫자, 특수문자를 모두 포함해야 합니다."
+                                    },
+                                    validate: (value) => {
+                                        if (value === "") {
+                                            return "비밀번호 취약: 비밀번호는 8~16자의 영문 대/소문자, 숫자, 특수문자를 모두 포함해야 합니다.";
+                                        }
+                                        if (value !== watchOldPassword) {
+                                            return "새 비밀번호와 현재 비밀번호가 일치합니다. 다시 확인해주세요.";
+                                        }
+                                    }
+                                })}
+                                type={newPasswordState.type}
+                                className={`w-full pr-10 rounded-[10px] mt-2 ${getInputErrorClassName(
+                                    errors.newPassword
+                                )}`}
+                                placeholder="새 비밀번호 입력"
+                            />
+                            {errors.newPassword && (
+                                <p className="mt-1 text-sm text-red-500">
+                                    {errors.newPassword.message}
+                                </p>
+                            )}
+                            <button
+                                type="button"
+                                className="absolute top-8 -translate-y-1/2 right-0 pr-3 flex items-center"
+                                onClick={handleNewToggle}
+                            >
+                                <img
+                                    src={handleNewEyeIconToggle()}
+                                    alt="toggle password visibility"
+                                    className="h-5 w-5"
+                                />
+                            </button>
+                        </div>
+                    </div>
                 </label>
-                <div className="relative">
-                    <input
-                        {...register("newPassword", {
-                            required: "비밀번호는 필수값입니다.",
-                            pattern: {
-                                value: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/,
-                                message:
-                                    "비밀번호 취약: 비밀번호는 8~16자의 영문 대/소문자, 숫자, 특수문자를 모두 포함해야 합니다."
-                            },
-                            validate: (value) =>
-                                value !== watchOldPassword ||
-                                "새 비밀번호와 현재 비밀번호가 일치합니다. 다시 확인해주세요."
-                        })}
-                        type={newPasswordState.type}
-                        className={`w-full pr-10 rounded-[10px] border-inputBorderColor ${getInputErrorClassName(
-                            errors.newPassword
-                        )}`}
-                        placeholder="새 비밀번호 입력"
-                    />
-                    <button
-                        type="button"
-                        className="absolute inset-y-0 right-0 pr-3 flex items-center"
-                        onClick={handleNewToggle}
-                    >
-                        <img
-                            src={handleNewEyeIconToggle()}
-                            alt="toggle password visibility"
-                            className="h-5 w-5"
-                        />
-                    </button>
-                </div>
-                {errors.newPassword && (
-                    <p className="mt-1 text-sm text-red-500">
-                        {errors.newPassword.message}
-                    </p>
-                )}
             </div>
 
             <div>
-                <label className="mb-2 block text-sm font-medium text-gray-700">
+                <label className="mb-2 block text-sm text-gray-700">
                     새 비밀번호 확인
+                    <div className="flex flex-col">
+                        <div className="relative">
+                            <input
+                                {...register("confirmPassword", {
+                                    required: "비밀번호는 필수값입니다.",
+                                    validate: (value) =>
+                                        value === watchPassword ||
+                                        "비밀번호가 일치하지 않습니다."
+                                })}
+                                type={confirmPasswordState.type}
+                                className={`w-full pr-10 rounded-[10px] mt-2 ${getInputErrorClassName(
+                                    errors.confirmPassword
+                                )}`}
+                                placeholder="새 비밀번호 확인"
+                            />
+                            {errors.confirmPassword && (
+                                <p className="mt-1 text-sm text-red-500">
+                                    {errors.confirmPassword.message}
+                                </p>
+                            )}
+                            <button
+                                type="button"
+                                className="absolute top-8 -translate-y-1/2 right-0 pr-3 flex items-center"
+                                onClick={handleConfirmToggle}
+                            >
+                                <img
+                                    src={handleConfirmEyeIconToggle()}
+                                    alt="toggle password visibility"
+                                    className="h-5 w-5"
+                                />
+                            </button>
+                        </div>
+                    </div>
                 </label>
-                <div className="relative">
-                    <input
-                        {...register("confirmPassword", {
-                            required: "비밀번호는 필수값입니다.",
-                            validate: (value) =>
-                                value === watchPassword ||
-                                "비밀번호가 일치하지 않습니다."
-                        })}
-                        type={confirmPasswordState.type}
-                        className={`w-full pr-10 rounded-[10px] border-inputBorderColor ${getInputErrorClassName(
-                            errors.confirmPassword
-                        )}`}
-                        placeholder="새 비밀번호 확인"
-                    />
-                    <button
-                        type="button"
-                        className="absolute inset-y-0 right-0 pr-3 flex items-center"
-                        onClick={handleConfirmToggle}
-                    >
-                        <img
-                            src={handleConfirmEyeIconToggle()}
-                            alt="toggle password visibility"
-                            className="h-5 w-5"
-                        />
-                    </button>
-                </div>
-                {errors.confirmPassword && (
-                    <p className="mt-1 text-sm text-red-500">
-                        {errors.confirmPassword.message}
-                    </p>
-                )}
             </div>
 
             <button
