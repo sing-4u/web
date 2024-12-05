b72593a41b5c32562c36bb0c17716ed2a0c7018c
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-12-05 17:29:54 +0900

    fix : Logo 추가

--- src/pages/NewPassword.tsx
+++ src/pages/NewPassword.tsx
@@ -8,6 +8,7 @@ import axiosInstance from "../utils/axiosInstance";
 import { ToastContainer } from "../components/ToastContainer";
 import Navbar from "../components/Navbar";
 import { useTitle } from "../hooks/useTitle";
+import Logo from "../components/Logo";
 
 interface PasswordProps {
     newPassword: string;
@@ -65,6 +66,12 @@ const NewPassword = () => {
             onSubmit={handleSubmit(onSubmit)}
             className="w-full max-w-[376px] mx-auto p-6"
         >
+            <div
+                onClick={() => navigate("/")}
+                className="w-[64px] h-[22.5px] cursor-pointer"
+            >
+                <Logo />
+            </div>
             <ToastContainer toasts={toasts} />
             <div className="">
                 <div className="text-2xl font-bold text-center mt-[22px]">
