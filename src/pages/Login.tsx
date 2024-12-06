6bbdffb9ddb8fd281b8e7f9494aae0e3a0f79a0b
GILPYO <rlfvy789@kakao.com>
2024-12-06 12:44:42 +0900

    fix: 로그인 에러메시지 통일(재)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -54,12 +54,12 @@ const Login = () => {
       if (axios.isAxiosError(error) && error.response?.status === 404) {
         setLoginState({
           loading: false,
-          error: "존재하지 않는 유저입니다.",
+          error: "이메일이나 비밀번호를 확인해주세요.",
         });
       } else if (error.response?.status === 401) {
         setLoginState({
           loading: false,
-          error: "비밀번호가 일치하지 않습니다.",
+          error: "이메일이나 비밀번호를 확인해주세요.",
         });
       } else {
         setLoginState({
