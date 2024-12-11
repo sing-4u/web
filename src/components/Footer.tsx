a5edb5ebbe0906caf5fa559c5017b3f6cd66209e
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-12-11 20:26:14 +0900

    fix : footer 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -26,11 +26,11 @@ const handleTermsClick = () => {
 
 const Footer = () => {
     return (
-        <div className="w-full bg-white mobile:min-w-[375px] min-h-[40px] md:flex md:justify-center mobile:mx-4 mobile:flex mobile:flex-col mobile:justify-center mobile:items-start tablet:flex tablet:justify-between tablet:items-center lg:w-full pc:w-full mobile:border-0 border-[0.5px] border-inputBorderColor pc:h-[40px] pc:items-center pc:justify-between fixed bottom-0 mobile:py-3">
+        <div className="w-full bg-white mobile:min-w-[375px] mobile:mx-4 mobile:flex mobile:flex-col mobile:justify-center mobile:items-start min-h-[40px] md:flex md:justify-center tablet:flex tablet:h-10 tablet:justify-between tablet:items-center lg:w-full pc:w-full mobile:border-0 border-[0.5px] border-inputBorderColor pc:h-10 pc:items-center pc:justify-between fixed bottom-0 mobile:py-3">
             <div className="flex mx-auto mobile:w-full w-[200px] h-[14px] font-bold text-[12px] leading-[14.32px] text-customGray gap-2 mobile:mx-6 mobile:my-[10px] mobie:h-[84px]">
                 <div
                     onClick={handlePrivacyClick}
-                    className="h-[14px] cursor-pointer font-bold text-[12px] leading-[14.32px] md:text-[11px] md:leading-[13.13px]"
+                    className="h-[14px] cursor-pointer font-bold mobile:text-xs leading-[14.32px] tablet:text-[11px] pc:text-[11px] md:leading-[13.13px]"
                 >
                     개인정보 처리 방침
                 </div>
@@ -38,14 +38,14 @@ const Footer = () => {
                 <div className="w-[1px] h-[14px] border border-customGray opacity-30"></div>
                 <div
                     onClick={handleTermsClick}
-                    className="h-[14px] cursor-pointer font-normal text-[12px] leading-[14.32px] md:text-[11px] md:leading-[13.13px]"
+                    className="h-[14px] cursor-pointer font-normal mobile:text-xs leading-[14.32px] tablet:text-[11px] pc:text-[11px] md:leading-[13.13px]"
                 >
                     이용약관
                 </div>
                 <div className="w-[1px] h-[14px] border border-customGray opacity-30"></div>
                 <div
                     onClick={handleInquiryClick}
-                    className="h-[14px] cursor-pointer font-normal text-[12px] leading-[14.32px] md:text-[11px] md:leading-[13.13px]"
+                    className="h-[14px] cursor-pointer font-normal mobile:text-xs leading-[14.32px] tablet:text-[11px] pc:text-[11px] md:leading-[13.13px]"
                 >
                     문의
                 </div>
