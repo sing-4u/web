3fdaad73c012e841a52ee75e2b4ce1629ed5ee3e
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-12-11 12:53:34 +0900

    fix : 검색바 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -179,28 +179,28 @@ export default function Home() {
         "mobile:w-[72px] mobile:h-[72px] mobile:py-[26px] mobile:px-[10px] mobile:text-[10px] mobile:font-semibold mobile:leading-[11.93px] bg-black text-white rounded-full flex items-center justify-center text-base hover:bg-gray-800 transition-colors";
 
     return (
-        <div className="w-full mx-auto p-6 space-y-4 pc:px-[191px]">
+        <div className="w-full space-y-4 ">
             <Navbar />
-            <div className="relative">
+            <div className="relative pc:px-[191px] mobile:px-6 tablet:px-[46px]">
                 <input
                     value={searchTerm}
                     onChange={handleSearchChange}
                     type="text"
                     placeholder="아티스트명을 검색해주세요"
-                    className="w-full border p-2 pl-10 rounded-[10px]"
+                    className="w-full border h-14 rounded-[10px] pl-[60px]"
                 />
                 <img
                     src={SearchIcon}
                     alt="Search Icon"
-                    className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5"
+                    className="absolute left-[215px] mobile:left-[46px] tablet:left-[70px] top-1/2 transform -translate-y-1/2 w-6 h-6"
                 />
             </div>
-            <div className="grid mobile:grid-cols-2 w-full gap-4 pc:grid-cols-4 tablet:grid-cols-3">
+            <div className="grid mobile:grid-cols-2 w-full gap-4 pc:grid-cols-4 tablet:grid-cols-3 pc:px-[191px] mobile:px-6 tablet:px-[46px]">
                 {users.map((user, index) => (
                     <div key={`${user.id}_${index}`} className="flex flex-col">
                         <div className="relative rounded-[20px] overflow-hidden">
                             <div className="absolute inset-0 bg-gradient-to-r from-[#7B92C7] via-[#7846DD] to-[#BB7FA0] rounded-[20px]" />
-                            <div className="relative m-[4px] bg-white rounded-[16px] overflow-hidden">
+                            <div className="relative m-[8px] bg-white rounded-[8px] overflow-hidden">
                                 <div
                                     className="relative aspect-square w-full"
                                     onClick={() => handleSongDetailClick(user)}
@@ -211,7 +211,7 @@ export default function Home() {
                                         className="w-full h-full object-cover"
                                     />
                                     {user.isOpened && (
-                                        <div className="absolute top-2 left-2 bg-gradient-to-br text-white from-[#7B92C7] via-[#7846DD] to-[#BB7FA0] text-xs font-bold py-1 px-2 rounded-md border border-black">
+                                        <div className="absolute top-2 left-2 bg-gradient-to-br text-white from-[#7B92C7] via-[#7846DD] to-[#BB7FA0] text-xs font-bold py-1 px-2 rounded-[4px]">
                                             신청곡 받는 중
                                         </div>
                                     )}
