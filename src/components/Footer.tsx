f0666173cdf6fcfe3dd93baa7e6fa358ab196a4d
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-12-11 17:45:20 +0900

    fix : footer 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -26,11 +26,11 @@ const handleTermsClick = () => {
 
 const Footer = () => {
     return (
-        <div className="w-full bg-white mobile:min-w-[375px] min-h-[40px] md:flex md:justify-center mobile:flex mobile:flex-col mobile:justify-center mobile:items-center tablet:flex tablet:justify-between tablet:items-center lg:w-full pc:w-[1440px] pc:h-[40px] pc:items-center pc:justify-between fixed bottom-0">
-            <div className="flex mx-auto w-[200px] h-[14px] font-bold text-[12px] leading-[14.32px] text-customGray gap-2 ">
+        <div className="w-full bg-white mobile:min-w-[375px] min-h-[40px] md:flex md:justify-center mobile:mx-4 mobile:flex mobile:flex-col mobile:justify-center mobile:items-start tablet:flex tablet:justify-between tablet:items-center lg:w-full pc:w-full mobile:border-0 border-[0.5px] border-inputBorderColor pc:h-[40px] pc:items-center pc:justify-between fixed bottom-0 mobile:py-3">
+            <div className="flex mx-auto mobile:w-full w-[200px] h-[14px] font-bold text-[12px] leading-[14.32px] text-customGray gap-2 mobile:mx-6 mobile:my-[10px] mobie:h-[84px]">
                 <div
                     onClick={handlePrivacyClick}
-                    className="w[42px] h-[14px] cursor-pointer font-bold text-[12px] leading-[14.32px] md:text-[11px] md:leading-[13.13px]"
+                    className="h-[14px] cursor-pointer font-bold text-[12px] leading-[14.32px] md:text-[11px] md:leading-[13.13px]"
                 >
                     개인정보 처리 방침
                 </div>
@@ -38,19 +38,19 @@ const Footer = () => {
                 <div className="w-[1px] h-[14px] border border-customGray opacity-30"></div>
                 <div
                     onClick={handleTermsClick}
-                    className="w[42px] h-[14px] cursor-pointer font-normal text-[12px] leading-[14.32px] md:text-[11px] md:leading-[13.13px]"
+                    className="h-[14px] cursor-pointer font-normal text-[12px] leading-[14.32px] md:text-[11px] md:leading-[13.13px]"
                 >
                     이용약관
                 </div>
                 <div className="w-[1px] h-[14px] border border-customGray opacity-30"></div>
                 <div
                     onClick={handleInquiryClick}
-                    className="w[42px] h-[14px] cursor-pointer font-normal text-[12px] leading-[14.32px] md:text-[11px] md:leading-[13.13px]"
+                    className="h-[14px] cursor-pointer font-normal text-[12px] leading-[14.32px] md:text-[11px] md:leading-[13.13px]"
                 >
                     문의
                 </div>
             </div>
-            <div className="w-[327px] lg:w-[230px] lg:text-[10px] lg:leading-[11.93px] h-[12px] pr-2.5 mx-auto text-center font-normal text-[10px] leading-[11.93px] text-customGray md:w-[230px] md:h-[12px] md:text-[10px] md:leading-[11.93px]">
+            <div className="mobile:mx-6 mobile:text-left w-[327px] lg:w-[230px] lg:text-[10px] lg:leading-[11.93px] h-[12px] pr-2.5 mx-auto text-center font-normal text-[10px] leading-[11.93px] text-customGray md:w-[230px] md:h-[12px] md:text-[10px] md:leading-[11.93px]">
                 <div>Copyright 2024 Sing4U All rights reserved.</div>
             </div>
         </div>
